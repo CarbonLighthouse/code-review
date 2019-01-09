098b90e8e970998b0c8ad7c3277a3fe10fdd1505
Kyle Ibrahim <kyle@carbonlighthouse.com>
2019-01-09 10:53:38 -0800

    Add interval batcher
    Update

--- main.py
+++ main.py
@@ -1,15 +1,56 @@
 import time
+from threading import Timer
+
+
+class IntervalBatcher:
+    def __init__(self, interval):
+        self.interval = interval
+        self.q = []
+        self.timer = Timer(self.interval, self.trigger)
+
+    def trigger(self):
+        self.flush()
+        self.timer = Timer(self.interval, self.trigger)
+        self.timer.start()
+
+
+    def flush(self):
+        print(self.q)
+        self.q = []
+
+    def start(self):
+        self.timer.start()
+
+    def stop(self):
+        self.timer.cancel()
+        self.trigger()
+
+    def put(self, item):
+        self.q.append(item)
 
 
 class Logger:
+    def __init__(self, interval):
+        self.batcher = IntervalBatcher(interval)
+
+
+    def start(self):
+        self.batcher.start()
+
+    def stop(self):
+        self.batcher.stop()
+
     def write_log(self, message):
-        print(message)
+        self.batcher.put(message)
 
 
 if __name__ == "__main__":
     # execute only if run as a script
-    logger = Logger()
+    logger = Logger(interval=1.0)
 
+    logger.start()
     for i in range(10):
         logger.write_log(f"Message {i}")
         time.sleep(.25)
+
+    logger.stop()
